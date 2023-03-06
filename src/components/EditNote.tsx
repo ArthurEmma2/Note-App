d9e5ba7a1454e477b7e7d5483219f3d8d804661e
Arthur Emmanuel <yhawnoworries@gmail.com>
2023-03-06 07:19:47 +0000

    update

--- src/components/EditNote.tsx
+++ src/components/EditNote.tsx
@@ -1,19 +1,24 @@
 import { NewNoteForm } from "./NewNoteForm";
-import { NoteData, Tag } from "../App";
+import { NoteData, Tag, Note } from '../App';
+import { useNote } from '../NoteLayout';
 type EditNoteProps = {
-  onSubmit: (data: NoteData) => void;
+  onSubmit: (id:string, data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
   availableTags: Tag[];
 };
 
 function EditNote({ onSubmit, onAddTag, availableTags }: EditNoteProps) {
+  const note = useNote()
   return (
     <>
       <h1 className="mb-4">Edit Note</h1>
       <NewNoteForm
+        title={note.title}
+        markdown={note.markdown}
+        tags={note.tags}
         onAddTag={onAddTag}
         availableTags={availableTags}
-        onSubmit={onSubmit}
+        onSubmit={data => onSubmit(note.id, data)}
       />
     </>
   );
